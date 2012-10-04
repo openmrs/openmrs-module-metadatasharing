e05704c9df073bd6dc85a341c7e69298e274a8f0
Ben Wolfe <benwolfe@gmail.com>
2012-10-04 12:01:22 -0400

    Fixing MDS to depend on ConceptPubSub fully - META-282 https://tickets.openmrs.org/browse/META-282

--- api-common/src/main/java/org/openmrs/module/metadatasharing/MetadataSharing.java
+++ api-common/src/main/java/org/openmrs/module/metadatasharing/MetadataSharing.java
@@ -1,5 +1,5 @@
 /**
-b * The contents of this file are subject to the OpenMRS Public License
+ * The contents of this file are subject to the OpenMRS Public License
  * Version 1.0 (the "License"); you may not use this file except in
  * compliance with the License. You may obtain a copy of the License at
  * http://license.openmrs.org
@@ -16,6 +16,7 @@ package org.openmrs.module.metadatasharing;
 import org.openmrs.api.context.Context;
 import org.openmrs.module.Module;
 import org.openmrs.module.ModuleFactory;
+import org.openmrs.module.conceptpubsub.ConceptPubSub;
 import org.openmrs.module.metadatasharing.api.MetadataSharingService;
 import org.openmrs.module.metadatasharing.converter.ConverterEngine;
 import org.openmrs.module.metadatasharing.handler.HandlerEngine;
@@ -187,10 +188,14 @@ public class MetadataSharing implements ApplicationContextAware {
 		}
 	}
 	
+	
+	/**
+	 * @return
+	 * @Deprecated use {@link ConceptPubSub#isAddLocalMappings()}
+	 */
+	@Deprecated
 	public boolean isAddLocalMappings() {
-		String addLocalMappings = Context.getAdministrationService().getGlobalProperty(
-		    MetadataSharingConsts.GP_ADD_LOCAL_MAPPINGS);
-		return Boolean.valueOf(addLocalMappings);
+		return ConceptPubSub.isAddLocalMappings();
 	}
 	
 	/**
