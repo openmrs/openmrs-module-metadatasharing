ed3b0b047c9f1f51bfc68e85835451c8b541603d
Rowan Seymour <rowanseymour@gmail.com>
2013-09-17 15:19:08 +0200

    Added failing unit test to demonstrate problem updating existing programs when package contains concepts

--- api-1.9/src/test/java/org/openmrs/metadatasharing/smoke/Concept19SmokeTest.java
+++ api-1.9/src/test/java/org/openmrs/metadatasharing/smoke/Concept19SmokeTest.java
@@ -13,10 +13,16 @@ package org.openmrs.metadatasharing.smoke;
 
 import java.io.InputStream;
 
+import org.junit.Assert;
 import org.junit.Test;
+import org.openmrs.Program;
+import org.openmrs.api.context.Context;
+import org.openmrs.module.metadatasharing.ImportConfig;
+import org.openmrs.module.metadatasharing.ImportMode;
 import org.openmrs.module.metadatasharing.MetadataSharing;
 import org.openmrs.module.metadatasharing.wrapper.PackageImporter;
 import org.openmrs.test.BaseModuleContextSensitiveTest;
+import org.openmrs.test.SkipBaseSetup;
 
 
 public class Concept19SmokeTest extends BaseModuleContextSensitiveTest {
@@ -28,5 +34,52 @@ public class Concept19SmokeTest extends BaseModuleContextSensitiveTest {
 		
 		metadataImporter.loadSerializedPackageStream(inputStream);
 		metadataImporter.importPackage();
+	}
+
+	@Test
+	@SkipBaseSetup
+	public void importProgramsAndConcepts() throws Exception {
+		executeDataSet(INITIAL_XML_DATASET_PACKAGE_PATH);
+		executeDataSet("conceptDatatypes.xml");
+		authenticate();
+
+		final String TB_PROGRAM_UUID = "9f144a34-3a4a-44a9-8486-6b7af6cc64f6";
+
+		// Verify that program doesn't exist
+		Program program = Context.getProgramWorkflowService().getProgramByUuid(TB_PROGRAM_UUID);
+		Assert.assertNull(program);
+
+		// Import package containing program
+		PackageImporter metadataImporter = MetadataSharing.getInstance().newPackageImporter();
+		metadataImporter.loadSerializedPackageStream(ClassLoader.getSystemResourceAsStream("packages/Programs_Test-4.zip"));
+		metadataImporter.setImportConfig(ImportConfig.valueOf(ImportMode.MIRROR));
+		metadataImporter.importPackage();
+
+		// Verify that program now exists and that details are correct from package
+		program = Context.getProgramWorkflowService().getProgramByUuid(TB_PROGRAM_UUID);
+		Assert.assertNotNull(program);
+		Assert.assertEquals("TB Program", program.getName());
+		Assert.assertEquals("Treatment for TB patients", program.getDescription());
+
+		// Modify program, save and flush session
+		program.setName("XXX");
+		program.setDescription("YYY");
+		Context.getProgramWorkflowService().saveProgram(program);
+		Context.flushSession();
+		Context.clearSession();
+
+		// Re-import package containing program
+		metadataImporter = MetadataSharing.getInstance().newPackageImporter();
+		metadataImporter.loadSerializedPackageStream(ClassLoader.getSystemResourceAsStream("packages/Programs_Test-4.zip"));
+		metadataImporter.setImportConfig(ImportConfig.valueOf(ImportMode.MIRROR));
+		metadataImporter.importPackage();
+		Context.flushSession();
+		Context.clearSession();
+
+		// Verify that program details are correct from package
+		program = Context.getProgramWorkflowService().getProgramByUuid(TB_PROGRAM_UUID);
+		Assert.assertNotNull(program);
+		Assert.assertEquals("TB Program", program.getName());  // FAILS !!!
+		Assert.assertEquals("Treatment for TB patients", program.getDescription());     // FAILS !!!
 	}
 }
