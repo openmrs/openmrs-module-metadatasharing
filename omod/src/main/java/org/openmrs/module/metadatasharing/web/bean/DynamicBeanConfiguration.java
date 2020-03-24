4cf796f0e850bb49854f261e12ad2f84e37a0c75
sherrif10 <sharfspace@gmail.com>
2020-03-24 14:24:03 +0300

    META-371 Remove bean definition for defaultAnnotationHandlerMapping
     https://issues.openmrs.org/browse/META-371

---
+++ omod/src/main/java/org/openmrs/module/metadatasharing/web/bean/DynamicBeanConfiguration.java
@@ -0,0 +1,35 @@
+package org.openmrs.module.metadatasharing.web.bean;
+
+import org.openmrs.api.context.Context;
+import org.openmrs.module.ModuleUtil;
+import org.openmrs.util.OpenmrsConstants;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.handler.AbstractHandlerMapping;
+
+@Configuration
+public class DynamicBeanConfiguration {
+	/**
+	 * The DefaultAnnotationHandlerMapping class was deprecated and eventually
+	 * removed in Spring 5 The recommended replacement class
+	 * RequestMappingHandlerMapping was introduced in Spring 3.1.0 which is not
+	 * available on OpenMRS platform versions 1.9.x and 1.1.0.x which run Spring
+	 * 3.0.5 That's why we can't just statically replace this class in the
+	 * webModuleApplicationContext.xml file.
+	 */
+	@Bean
+	public AbstractHandlerMapping getHandlerMapping() throws Exception {
+
+		Class<?> clazz;
+		if (ModuleUtil.compareVersion(OpenmrsConstants.OPENMRS_VERSION_SHORT, "2.4") < 0) {
+			clazz = Context.loadClass("org.springframework.web.servlet.mvc.annotation.DefaultAnnotationHandlerMapping");
+		} else {
+			clazz = Context
+					.loadClass("org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping");
+		}
+
+		return (AbstractHandlerMapping) clazz.newInstance();
+	}
+
+}
+
